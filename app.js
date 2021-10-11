925062f84190b3d323287408f1bed54a236d7edf
Bryant-Wang24 <485969746@qq.com>
2021-10-11 23:56:09 +0800

    多个表的关联查询完成

--- app.js
+++ app.js
@@ -7,7 +7,23 @@ const ArticleModel = require('./model/article.js')
 
 
 // 查询文章信息 并显示文章的分类 以及文章的作者信息
+
 //两个表的关联查询 
+// ArticleModel.aggregate([
+//     {
+//         $lookup: {
+//             from: "articlecate",//与articlecate表关联
+//             localField: "cid",//关联条件
+//             foreignField: '_id',//article表的cid和articlecate的_id关联
+//             as: "cate"
+//         }
+//     }
+// ], (err, data) => {
+//     console.log(data[1].cate);
+// })
+
+
+// 三个表的关联查询
 ArticleModel.aggregate([
     {
         $lookup: {
@@ -16,7 +32,15 @@ ArticleModel.aggregate([
             foreignField: '_id',//article表的cid和articlecate的_id关联
             as: "cate"
         }
+    },
+    {
+        $lookup: {
+            from: "user",//与user表关联
+            localField: "author_id",//关联条件
+            foreignField: '_id',
+            as: "user"
+        }
     }
 ], (err, data) => {
-    console.log(data[1].cate);
+    console.log(JSON.stringify(data));
 })
